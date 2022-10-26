3d0860fa86fee93a496d29a69cd9b1b366e7266b
Adebayo Samuel <eladebayoor@gmail.com>
2022-10-26 03:33:44 +0100

    [fix]: CORS enabled

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,17 @@ import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
-
+from flask_cors import CORS, cross_origin
+from flasgger import Swagger
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
+swagger = Swagger(app)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+cors = CORS(app, resources={
+            r'/*': {'origins': os.getenv('HBNB_API_HOST', '0.0.0.0')}})
 app.register_blueprint(app_views)
 
 
@@ -26,6 +34,14 @@ def page_404_not_found(e):
     """method for 404 errors.
     """
     return ({'error': 'Not found'}), 404
+
+
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
 
 
 if __name__ == "__main__":
