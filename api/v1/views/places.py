ff459f16aa4715cd596168cfdadf75661a15505f
Adebayo Samuel <eladebayoor@gmail.com>
2022-10-26 02:49:52 +0100

    [fix]: handle missing email and password on post

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,46 +5,44 @@
 from flask import abort, Flask, jsonify, request
 from api.v1.views import app_views
 from models import storage
-from models.user import User
+from models.amenity import Amenity
 
 
-@app_views.route("/users", methods=["GET", "POST"], strict_slashes=False)
-def handle_users():
+@app_views.route("/amenities", methods=["GET", "POST"], strict_slashes=False)
+def handle_amenities():
     """
         Method to return a JSON representation of all states
     """
     if request.method == 'GET':
-        users = storage.all(User)
-        all_users = []
-        for user in users.values():
-            all_users.append(user.to_dict())
-        return jsonify(all_users)
+        amenities = storage.all(Amenity)
+        all_amenities = []
+        for amenity in amenities.values():
+            all_amenities.append(amenity.to_dict())
+        return jsonify(all_amenities)
     elif request.method == 'POST':
         post = request.get_json()
         if post is None or type(post) != dict:
             return jsonify({'error': 'Not a JSON'}), 400
-        elif post.get('email') is None:
-            return jsonify({'error': 'Missing email'}), 400
-        elif post.get('password') is None:
-            return jsonify({'error': 'Missing password'}), 400
-        new_user = User(**post)
-        new_user.save()
-        return jsonify(new_user.to_dict()), 201
+        elif post.get('name') is None:
+            return jsonify({'error': 'Missing name'}), 400
+        new_amenity = Amenity(**post)
+        new_amenity.save()
+        return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route("/users/<user_id>", methods=["GET", "PUT", "DELETE"],
+@app_views.route("/amenities/<amenity_id>", methods=["GET", "PUT", "DELETE"],
                  strict_slashes=False)
-def handle_user_by_id(user_id):
+def handle_amenity_by_id(amenity_id):
     """
         Method to return a JSON representation of a state
     """
-    user_by_id = storage.get(User, user_id)
-    if user_by_id is None:
+    amenity_by_id = storage.get(Amenity, amenity_id)
+    if amenity_by_id is None:
         abort(404)
     elif request.method == 'GET':
-        return jsonify(user_by_id.to_dict())
+        return jsonify(amenity_by_id.to_dict())
     elif request.method == 'DELETE':
-        storage.delete(user_by_id)
+        storage.delete(amenity_by_id)
         storage.save()
         return jsonify({}), 200
     elif request.method == 'PUT':
@@ -53,6 +51,6 @@ def handle_user_by_id(user_id):
             return jsonify({'message': 'Not a JSON'}), 400
         for key, value in put.items():
             if key not in ['id', 'created_at', 'updated_at']:
-                setattr(user_by_id, key, value)
+                setattr(amenity_by_id, key, value)
         storage.save()
-        return jsonify(user_by_id.to_dict()), 200
+        return jsonify(amenity_by_id.to_dict()), 200
